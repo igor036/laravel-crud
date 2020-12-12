3eb2774471898c5f83dfa500344bf40c2d5e0c82
igor036 <igorjoaquim.pg@gmail.com>
2020-12-12 09:03:17 -0300

    add fontawesome and bootstrap

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -2,20 +2,21 @@
 
 namespace Database\Seeders;
 
-use Carbon\Carbon;
-
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
+class ContactSeeder extends Seeder {
 
-class ContactSeeder extends Seeder
-{
-    public function run()
-    {
+    public function run() {
         DB::table('contact')->insert([
             'name' => 'Alice',
             'email' => 'liceu.l@gmail.com',
             'phone' => '778457856589'
+        ]);
+        DB::table('contact')->insert([
+            'name' => 'Igor',
+            'email' => 'igorjoaquim.pg@gmail.com',
+            'phone' => '79999901734'
         ]);
     }
 }
--- resources/views/contact/list.blade.php
+++ resources/views/contact/list.blade.php
@@ -1,42 +1,49 @@
 @extends('layout')
 @section('content')
-    <div>
+    <div  class="container mt-5">
 
         <h1>Contact List</h1>
         <p>
             <a href="{{ url('contact/create') }}">New Contact</a>
         </p>
 
-        <table>
-            <thead>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Actions</th>
-            </thead>
-            <tbody>
-                @if (count($contacts) === 0)
-                    <tr>
-                        <td colspan="4">You contact list is empty!</td>
-                    </tr>
-                @else
-                    @foreach ($contacts as $contact)
-                    <tr>
-                        <td>{{$contact->name}} </td>
-                        <td>{{$contact->email}}</td>
-                        <td>{{$contact->phone}}</td>
-                        <td>
-                            <button type="button" onclick="window.location='contact/edit/{{ $contact->id }}' ">Edit</button>
+        <div class="d-flex justify-content-center mt-5">
+            <table class="table table-striped">
+                <thead >
+                    <th class="col">Name</th>
+                    <th class="col">Email</th>
+                    <th class="col">Phone</th>
+                    <th class="col">Actions</th>
+                </thead>
+                <tbody>
+                    @if (count($contacts) === 0)
+                        <tr>
+                            <td colspan="4">You contact list is empty!</td>
+                        </tr>
+                    @else
+                        @foreach ($contacts as $contact)
                             <form action="contact/destroy/{{ $contact->id }}" method="post">
                                 @csrf @method('delete')
-                                <button>delete</button>
+                                <tr>
+                                    <td>{{$contact->name}} </td>
+                                    <td>{{$contact->email}}</td>
+                                    <td>{{$contact->phone}}</td>
+                                    <td>
+                                        <button class="btn btn-sm btn-primary" type="button" onclick="window.location='contact/edit/{{ $contact->id }}' ">
+                                            <i class="fas fa-edit"></i>
+                                        </button>
+                                        <button class="btn btn-sm btn-danger">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </button>
+                                    </td>
+                                </tr>
                             </form>
-                        </td>
-                    </tr>
-                    @endforeach
-                @endif
-            </tbody>
-        </table>
+                        @endforeach
+                    @endif
+                </tbody>
+            </table>
+        </div>
+
     </div>
 
 @endsection
