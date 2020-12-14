fd7f16ebf80d3ffec9f0e4a6d12b5ff026798de8
igor036 <igorjoaquim.pg@gmail.com>
2020-12-14 18:06:16 -0300

    creating ContractUpdateFeatureTest

---
+++ tests/Feature/Http/Controller/Contact/ContractUpdateFeatureTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature\Http\Controller\Contact;
+
+use App\Models\Contact;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class ContractUpdateFeatureTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_should_update_a_contact_with_success()
+    {
+
+        $contact = ContactFeatureTestUtil::mokeContactInstance(true);
+        $contact->name = 'Igor Joaquim';
+
+        $response = $this->put('contact/'.$contact->id, $contact->getAttributes());
+        $response->assertStatus(Response::HTTP_FOUND);
+
+        $queryContact = Contact::find($contact->id);
+        ContactFeatureTestUtil::assertContact($contact, $queryContact);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_should_return_a_invalid_name_message()
+    {
+        $contact = ContactFeatureTestUtil::mokeContactInstance(true);
+        $contact->name = '';
+
+        $response = $this->put('contact/'.$contact->id, $contact->getAttributes());
+        $response->assertSessionHasErrors(['name']);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_should_return_a_invalid_email_message()
+    {
+        $contact = ContactFeatureTestUtil::mokeContactInstance(true);
+        $contact->email = '';
+
+        $response = $this->put('contact/'.$contact->id, $contact->getAttributes());
+        $response->assertSessionHasErrors(['email']);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_should_return_a_invalid_phone_message()
+    {
+        $contact = ContactFeatureTestUtil::mokeContactInstance(true);
+        $contact->phone = '';
+
+        $response = $this->put('contact/'.$contact->id, $contact->getAttributes());
+        $response->assertSessionHasErrors(['phone']);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_should_return_a_invalid_message_for_all_properties()
+    {
+        $contact = ContactFeatureTestUtil::mokeContactInstance(true);
+        $contact->name = '';
+        $contact->email = '';
+        $contact->phone = '';
+
+        $response = $this->put('contact/'.$contact->id, $contact->getAttributes());
+        $response->assertSessionHasErrors(['email', 'name', 'phone']);
+    }
+}
