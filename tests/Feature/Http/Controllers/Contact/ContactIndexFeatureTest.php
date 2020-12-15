1adb29402da131846c53f3963442805712b30a72
igor036 <igorjoaquim.pg@gmail.com>
2020-12-15 10:39:49 -0300

    reactoring ContactIndexFeatureTest

--- tests/Feature/Http/Controllers/Contact/ContactIndexFeatureTest.php
+++ tests/Feature/Http/Controllers/Contact/ContactIndexFeatureTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature\Http\Controllers\Contact;
 
+use App\Http\Controllers\ContactController;
 use Tests\TestCase;
 
 use Illuminate\Http\Response;
+use Illuminate\Testing\TestResponse;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ContactIndexFeatureTest extends TestCase
@@ -18,42 +20,42 @@ class ContactIndexFeatureTest extends TestCase
      */
     public function it_should_render_index_without_contacts() {
 
-        $response = $this->get('contact');
-        $contacts = $response->viewData('contacts');
+        $response   = $this->get('contact');
+        $pagination = $response->viewData('pagination');
 
         $response->assertStatus(Response::HTTP_OK);
-        $this->assertEquals(0, $contacts->count());
+        $this->assertEquals(0, $pagination->count());
     }
 
     /**
      * @test
      * @return void
      */
-    public function it_should_render_index_with_only_one_contact() {
-
-        ContactFeatureTestUtil::mokeContactList(1, true);
-
-        $response = $this->get('contact');
-        $contacts = $response->viewData('contacts');
-
-        $response->assertStatus(Response::HTTP_OK);
-        $this->assertEquals(1, $contacts->count());
-
-    }
-
-    /**
-     * @test
-     * @return void
-     */
-    public function it_should_render_index_with_more_one_contacts() {
-
+    public function it_should_render_index_with_only_one_page() {
         ContactFeatureTestUtil::mokeContactList(10, true);
+        $response = $this->get('contact');
+        $this->assertIndexPaginationResponse($response, 1, 1);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_should_render_index_on_two_pages() {
+        ContactFeatureTestUtil::mokeContactList(20, true);
 
         $response = $this->get('contact');
-        $contacts = $response->viewData('contacts');
+        $this->assertIndexPaginationResponse($response, 2, 1);
 
+        $response = $this->get('contact?contacts_page=2');
+        $this->assertIndexPaginationResponse($response, 2, 2);
+    }
+
+    private function assertIndexPaginationResponse(TestResponse $response, int $expectedLastPage, int $expectedCurrentPage) {
+        $pagination = $response->viewData('pagination');
         $response->assertStatus(Response::HTTP_OK);
-        $this->assertEquals(10, $contacts->count());
-
+        $this->assertEquals($expectedLastPage, $pagination->lastPage());
+        $this->assertEquals($expectedCurrentPage, $pagination->currentPage());
+        $this->assertEquals(ContactController::CONTACTS_PER_PAGE, $pagination->perPage());
     }
 }
